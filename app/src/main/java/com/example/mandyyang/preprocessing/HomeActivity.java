1d8472dc8348262ae326def8ad2741dc008b4554
楊楊乘ㄧ <mandyyang@yangyangchengyi1de-MacBook-Pro.local>
2017-10-14 16:01:50 +0800

    Initial commit

---
+++ app/src/main/java/com/example/mandyyang/preprocessing/HomeActivity.java
@@ -0,0 +1,72 @@
+package com.example.mandyyang.preprocessing;
+
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class HomeActivity extends AppCompatActivity {
+
+    //MEAN_BLUR
+    public static final int MEAN_BLUR=1;
+    //GAUSSIAN_BLUR
+    public static final int GAUSSIAN_BLUR=2;
+    //MEDIAN_BLUR
+    public static final int MEDIAN_BLUR=3;
+    //SHARPEN
+    public static final int SHARPEN = 4;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        //bMean
+        Button bMean;
+        bMean = (Button)findViewById(R.id.bMean);
+        bMean.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                i.putExtra("ACTION_MODE", MEAN_BLUR);
+                startActivity(i);
+            }
+        });
+
+        //bGassian
+        Button bGassian;
+
+        bGassian = (Button)findViewById(R.id.bGassian);
+        bGassian.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                i.putExtra("ACTION_MODE", GAUSSIAN_BLUR);
+                startActivity(i);
+            }
+        });
+
+    //bMedian
+        Button bMedian = (Button)findViewById(R.id.bMedian);
+        bMedian.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), MainActivity.class); i.putExtra("ACTION_MODE", MEDIAN_BLUR);
+                startActivity(i);
+            } });
+
+
+     //SHARPEN
+        Button bSharpen;
+        bSharpen = (Button)findViewById(R.id.bSharpen); bSharpen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), MainActivity.class); i.putExtra("ACTION_MODE", SHARPEN);
+                startActivity(i);
+            } });
+
+    }
+}
---
+++ app/src/main/java/com/example/mandyyang/preprocessing/MainActivity.java
@@ -0,0 +1,183 @@
+package com.example.mandyyang.preprocessing;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String TAG = "OpenCV";
+    private final int SELECT_PHOTO=1;
+    private ImageView ivImage, ivImageProcessed;
+    Mat src, src_gray;
+    static int ACTION_MODE=0;
+    static int REQUEST_READ_EXTERNAL_STORAGE=0;
+    static boolean read_external_storage_granted=false;
+
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch(status){
+                case LoaderCallbackInterface.SUCCESS:{
+                    Log.i(TAG, "OpenCV Success!");
+                    break;
+                }
+                default:{
+                    Log.i(TAG, "OpenCV Fail!");
+                    super.onManagerConnected(status);
+                    break;
+                }
+            }
+
+        }
+    };
+
+    public void onResume(){
+        super.onResume();
+        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION, this, mLoaderCallback);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Add a left arrow on Action Bar to return to the previous Activity
+        assert getSupportActionBar() != null;
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        ivImage = (ImageView)findViewById(R.id.ivImage);
+        ivImageProcessed = (ImageView)findViewById(R.id.ivImageProcessed);
+        Intent intent = getIntent();
+
+        if(intent.hasExtra("ACTION_MODE")){
+            ACTION_MODE = intent.getIntExtra("ACTION_MODE", 0);
+        }
+
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            Log.i("permission", "request READ_EXTERNAL_STORAGE");
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                    REQUEST_READ_EXTERNAL_STORAGE);
+        }else {
+            Log.i("permission", "READ_EXTERNAL_STORAGE already granted");
+            read_external_storage_granted = true;
+        }
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_load_image && read_external_storage_granted) {
+            Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+            photoPickerIntent.setType("image/*");
+            startActivityForResult(photoPickerIntent, SELECT_PHOTO);
+
+            return true;
+        }else if(!read_external_storage_granted) {
+            Log.i("permission", "READ_EXTERNAL_STORAGE denied");
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
+        //Put it there, just in case:)
+        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
+
+        switch(requestCode) {
+            case SELECT_PHOTO:
+                if(resultCode == RESULT_OK && read_external_storage_granted) {
+                    try {
+                        final Uri imageUri = imageReturnedIntent.getData();
+                        final InputStream imageStream = getContentResolver().openInputStream(imageUri);
+                        final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
+                        src = new Mat(selectedImage.getHeight(), selectedImage.getWidth(), CvType.CV_8UC4);
+                        Utils.bitmapToMat(selectedImage, src);
+                        src_gray = new Mat(selectedImage.getHeight(), selectedImage.getWidth(), CvType.CV_8UC1);
+                        switch (ACTION_MODE) {
+
+                            //MEAN_BLUR
+                            case HomeActivity.MEAN_BLUR:
+                                Imgproc.blur(src, src, new Size(9, 9));
+                                break;
+
+                            //GAUSSIAN_BLUR 高斯模糊
+                            case HomeActivity.GAUSSIAN_BLUR:
+                                Imgproc.GaussianBlur(src, src, new Size(3, 3), 0);
+                                break;
+
+                            //MEDIAN_BLUR 中位數模糊
+                            case HomeActivity.MEDIAN_BLUR:
+                                Imgproc.medianBlur(src, src, 9);
+                                break;
+                            //SHARPEN 銳利化
+                            case HomeActivity.SHARPEN:
+                                Mat kernel = new Mat(3, 3, CvType.CV_16SC1);
+                                //int[] values = {0, -1, 0, -1, 5, -1, 0, -1, 0};
+                                //Log.d("imageType", CvType.typeToString(src.type()) + "");
+                                kernel.put(0, 0, 0, -1, 0, -1, 5, -1, 0, -1, 0);
+                                Imgproc.filter2D(src, src, src_gray.depth(), kernel);
+                                break;
+
+                        }
+
+                        Bitmap processedImage = Bitmap.createBitmap(src.cols(), src.rows(), Bitmap.Config.ARGB_8888);
+                        Log.i("imageType", CvType.typeToString(src.type()) + "");
+                        Utils.matToBitmap(src, processedImage);
+                        ivImage.setImageBitmap(selectedImage);
+                        ivImageProcessed.setImageBitmap(processedImage);
+                        Log.i("process", "process done");
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                }
+                break;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[], @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_READ_EXTERNAL_STORAGE) {
+            // If request is cancelled, the result arrays are empty.
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // permission was granted
+                Log.i("permission", "READ_EXTERNAL_STORAGE granted");
+                read_external_storage_granted = true;
+            } else {
+                // permission denied
+                Log.e("permission", "READ_EXTERNAL_STORAGE refused");
+                read_external_storage_granted = false;
+            }
+        }
+    }
+}
